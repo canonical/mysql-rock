7178e188b5a45035a2dc4c3cb413b896562f0416
Sinclert Pérez <sinclert.perez@canonical.com>
2026-01-20 10:59:22 +0100

    Simplify password generation

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -166,7 +166,7 @@ docker_setup_db() {
 	fi
 	# Generate random root password
 	if [ -n "$MYSQL_RANDOM_ROOT_PASSWORD" ]; then
-		export MYSQL_ROOT_PASSWORD="$(pwgen -1 32)"
+		export MYSQL_ROOT_PASSWORD="$(openssl rand -base64 32)"
 		mysql_note "GENERATED ROOT PASSWORD: $MYSQL_ROOT_PASSWORD"
 	fi
 	# Sets root password and creates root users for non-localhost hosts
